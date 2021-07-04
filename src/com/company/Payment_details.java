6a8fa4e4056e07a72f277bbc84942c84ca200718
dahamvin <dahamvin2003@gmail.com>
2021-07-04 18:26:21 +0530

    First commit! Let's Goooo

---
+++ src/com/company/Payment_details.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Payment_details {
+    int price1 = 10000;
+    int price2 = 15000;
+    int price3 = 12000;
+    int price4 = 13000;
+
+
+
+    public void print1(){
+
+        System.out.println("Amount: Rs."+price1);
+    }
+    public void print2(){
+        System.out.println("Amount: Rs."+price2);
+    }
+    public void print3() {
+        System.out.println("Amount: Rs." + price3);
+    }
+}
