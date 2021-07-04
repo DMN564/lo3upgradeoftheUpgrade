2e0cc93af467f2170c27f977ac00836ab1ed6b36
dahamvin <dahamvin2003@gmail.com>
2021-07-04 18:20:55 +0530

    First commit! Let's Goooo

---
+++ src/com/company/Patient_info.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Patient_info {
+    String patient_name;
+    String patient_no;
+    int patient_height;
+    String password;
+    int patient_weight;
+    int procedure_no;
+    String patientUsername;
+    String gender;
+    int age;
+
+    public void procedure(){
+        System.out.println("Procedure details: Body checkup");
+    }
+    public void procedure1(){
+        System.out.println("Procedure details: Vaccination");
+    }
+    public void procedure2(){
+        System.out.println("Procedure details: Surgery");
+    }
+}
